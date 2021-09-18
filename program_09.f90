bec5f057d2313f124e4d8c4a22ebdea6324f0987
WoW <rickenbacker990@gmail.com>
2021-09-18 13:30:13 -0400

    Updates 9/18/2021

--- program_09.f90
+++ program_09.f90
@@ -4,6 +4,25 @@
 program format_test
 	implicit none
 	
+	integer :: num, cups
+	real :: liters, quarts
+
+
+! This do loop formats the multiplications of 7 from 1-12	
+	do num = 1,12
+	  print 100, num, num * 7
+	  100 format(i2,' * 7 = ', i3)
+	end do
+
+! A nice layout for cups to liters/quarts table
+	print "(/a18)", "Cups Liters Quarts"
+	do cups = 1,10
+	  liters =  cups * 0.236
+	  quarts = cups * 0.208
+	  print 200, cups, liters, quarts
+	  200 format(" ", i3, 2x, f5.3, 2x, f5.3)
+	end do	
+
 
 
 
---
+++ program_10.f90
@@ -0,0 +1,14 @@
+! This fortran script details more sophisticated uses of the
+! the string data type
+
+program string_test
+	implicit none
+
+
+	
+
+
+
+
+
+end program string_test
