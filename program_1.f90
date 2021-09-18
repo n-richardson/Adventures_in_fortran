72de71b30cb8e17623b92cdc6af4653c28535124
WoW <rickenbacker990@gmail.com>
2021-09-18 13:17:59 -0400

    Deleting old files

--- program_1.f90
+++
@@ -1,9 +0,0 @@
-program fortrantut
-	implicit none
-	character*20 :: name
-        character (len = 20) :: f_name, l_name
-	print *, "what's your name "
-	read *, f_name, l_name
-	print *, "Hello ", trim(f_name)," ",trim(l_name)
-end program fortrantut
-
